f018d95302de805362d25cb06831880e7da147f1
Bruno Lemos <brunohplemos@gmail.com>
2016-07-31 19:22:27 -0300

    1.8.5

--- package.js
+++ package.js
@@ -1,18 +1,17 @@
 Package.describe({
   name: 'brunolemos:parse-sdk',
-  version: '1.6.7',
+  version: '1.8.5',
   summary: 'Parse SDK for Meteor',
   git: 'https://github.com/brunolemos/parse-sdk.git',
   documentation: 'README.md'
 });
 
 Package.onUse(function(api) {
-  api.versionsFrom('METEOR@0.9.0');
-  api.addFiles('parse-sdk.js');
+  api.mainModule('parse-sdk.js');
 });
 
 Package.onTest(function(api) {
   api.use('tinytest');
   api.use('brunolemos:parse-sdk');
-  api.addFiles('parse-sdk-tests.js');
+  api.mainModule('parse-sdk-tests.js');
 });
--- parse-sdk-tests.js
+++ parse-sdk-tests.js
@@ -1,3 +1,5 @@
+var Tinytest = require('meteor/tinytest').Tinytest;
+
 Tinytest.add('global-object', function (test) {
   test.equal(typeof Parse, 'object');
 });
